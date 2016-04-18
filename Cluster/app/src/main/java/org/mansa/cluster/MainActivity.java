f8e03ead16eba96150fa0825e7378416e4542c1d
martnga <mtnjoroge@gmail.com>
2016-04-18 10:51:39 +0300

    Successful

--- Cluster/app/src/main/java/org/mansa/cluster/MainActivity.java
+++ Cluster/app/src/main/java/org/mansa/cluster/MainActivity.java
@@ -3,11 +3,24 @@ package org.mansa.cluster;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import co.moonmonkeylabs.realmsfrestaurantdata.SFRestaurantDataLoader;
+import co.moonmonkeylabs.realmsfrestaurantdata.SFRestaurantModule;
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_main );
+        super.onCreate(savedInstanceState);
+        // Sets default realm with sample data module
+        Realm.setDefaultConfiguration(new RealmConfiguration
+                .Builder(this)
+                .setModules( Realm.getDefaultModule(), new SFRestaurantModule())
+                .build());
+        // Loads and adds sample data to realm
+        new SFRestaurantDataLoader().loadBusinessSmallDataSet(this);
+        // Sets layout with map fragment
+        setContentView(R.layout.activity_main);
     }
 }
